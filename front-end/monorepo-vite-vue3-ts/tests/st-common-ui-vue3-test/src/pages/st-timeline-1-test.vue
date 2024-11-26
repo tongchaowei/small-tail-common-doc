eeead73637cd8ffdbe40b335a2eaa723bf4419c5
small tail <1974498278@qq.com>
2024-11-27 02:31:45 +0800

    修改：基于 StSwiper StSwiperSlide 重构 StTimeline1 组件

--- front-end/monorepo-vite-vue3-ts/tests/st-common-ui-vue3-test/src/pages/st-timeline-1-test.vue
+++ front-end/monorepo-vite-vue3-ts/tests/st-common-ui-vue3-test/src/pages/st-timeline-1-test.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
-import {StTimeline1} from "st-common-ui-vue3"
+import {StTimeline1, StTimelineItem1} from "st-common-ui-vue3"
 import {timelineData} from './st-timeline-1-test-data'
 </script>
 
 <template>
   <div class="st-timeline-1-test">
     <st-timeline1
-      :data="timelineData"
-      :timelinePerView="1"
-      :timeline-per-view-react-screen="1"
-    ></st-timeline1>
+      :timeline-data="[...timelineData, ...timelineData].map(item => item.title)"
+      :timeline-per-view="1"
+      :timeline-text-max-line="2"
+    >
+      <StTimelineItem1
+        v-for="(item, index) in [...timelineData, ...timelineData]"
+        :key="index"
+        :idx="index"
+        :bg-img-src="item.bgImgSrc"
+        :title="item.title"
+        :desc="item.desc"
+      ></StTimelineItem1>
+    </st-timeline1>
   </div>
 </template>
 
