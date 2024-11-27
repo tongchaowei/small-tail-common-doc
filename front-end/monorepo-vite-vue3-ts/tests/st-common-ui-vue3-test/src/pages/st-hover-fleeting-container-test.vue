a77d401b2c3563da57f486c6ef44b8de195981d4
small tail <1974498278@qq.com>
2024-11-27 14:21:56 +0800

    新增：StHoverFleetingContainer 鼠标悬浮流光边框组件

---
+++ front-end/monorepo-vite-vue3-ts/tests/st-common-ui-vue3-test/src/pages/st-hover-fleeting-container-test.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {StHoverFleetingContainer} from "st-common-ui-vue3"
+</script>
+
+<template>
+  <div class="st-hover-fleeting-container-test w-full h-screen bg-[#333] flex justify-center items-center">
+    <div class="st-hover-fleeting-container-box w-sm h-sm">
+      <StHoverFleetingContainer
+        :shine="false"
+        :blur-border="true"
+      >
+        <div class="w-full h-full flex justify-center items-center text-white font-bold text-lg">StHoverFleetingContainer</div>
+      </StHoverFleetingContainer>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
